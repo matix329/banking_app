d71bfcbd3081f813d3a3d241b29a3e91abf8d14a
matix329 <mateusz.siejo@wp.pl>
2024-11-20 22:17:12 +0100

    Delete old tests

--- tests/test_pin_hasher.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from banking_core.services.pin_hasher import PinHasher
-
-def test_pin_hashing():
-    pin = "1234"
-    hashed_pin = PinHasher.hash_pin(pin)
-
-    assert PinHasher.check_pin(hashed_pin, pin) == True
-    assert PinHasher.check_pin(hashed_pin, "wrong_pin") == False

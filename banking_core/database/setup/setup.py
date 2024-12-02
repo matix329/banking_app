ee2b5e563cba29a85ad101e7c6e6cca7348ef6e8
matix329 <mateusz.siejo@wp.pl>
2024-12-02 18:56:47 +0100

    Revamp database schema and optimize structure (#6)

--- banking_core/database/setup/setup.py
+++ banking_core/database/setup/setup.py
@@ -3,36 +3,60 @@ import psycopg2
 def setup_database(connection):
     try:
         cursor = connection.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS card (
-                          id SERIAL PRIMARY KEY, 
-                          number TEXT UNIQUE
-                       );''')
-        cursor.execute('''CREATE TABLE IF NOT EXISTS account (
-                          id SERIAL PRIMARY KEY, 
-                          card_id TEXT, 
-                          pin TEXT, 
-                          balance NUMERIC(10, 2) DEFAULT 0, 
-                          currency CHAR(3), 
-                          failed_attempts INTEGER DEFAULT 0, 
-                          locked BOOLEAN DEFAULT FALSE, 
-                          FOREIGN KEY (card_id) REFERENCES card(number)
-                       );''')
-        cursor.execute('''CREATE TABLE IF NOT EXISTS transactions (
-                          id SERIAL PRIMARY KEY,
-                          account_id INTEGER, 
-                          transaction_type TEXT, 
-                          amount NUMERIC(10, 2), 
-                          date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-                          FOREIGN KEY (account_id) REFERENCES account(id)
-                       );''')
-        cursor.execute('''CREATE TABLE IF NOT EXISTS daily_limits (
-                          id SERIAL PRIMARY KEY, 
-                          account_id INTEGER,
-                          daily_limit NUMERIC(10, 2), 
-                          set_date TEXT,
-                          changes_today INTEGER DEFAULT 0,
-                          FOREIGN KEY (account_id) REFERENCES account(id)
-                       );''')
+        cursor.execute('''  CREATE TABLE IF NOT EXISTS customer (
+                            id SERIAL PRIMARY KEY,
+                            first_name VARCHAR(100) NOT NULL,
+                            last_name VARCHAR(100) NOT NULL,
+                            email VARCHAR(50) UNIQUE NOT NULL,
+                            phone VARCHAR(9) UNIQUE,
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                        );''')
+        cursor.execute('''  CREATE TABLE IF NOT EXISTS account (
+                            id SERIAL PRIMARY KEY, 
+                            customer_id INTEGER NOT NULL,
+                            account_number VARCHAR(7) UNIQUE NOT NULL,
+                            password TEXT NOT NULL, 
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+                            FOREIGN KEY (customer_id) REFERENCES customer(id) ON DELETE CASCADE
+                        );''')
+        cursor.execute('''  CREATE TABLE IF NOT EXISTS card (
+                            id SERIAL PRIMARY KEY,
+                            account_id INTEGER NOT NULL,
+                            sub_account_id INTEGER,
+                            card_number VARCHAR(16) UNIQUE NOT NULL,
+                            expiry_date DATE NOT NULL,
+                            cvv VARCHAR(3) NOT NULL,
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                            FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE CASCADE,
+                            FOREIGN KEY (sub_account_id) REFERENCES sub_account(id) ON DELETE CASCADE
+                        );''')
+        cursor.execute('''  CREATE TABLE IF NOT EXISTS transactions (
+                            id SERIAL PRIMARY KEY,
+                            account_id INTEGER NOT NULL, 
+                            transaction_type TEXT, 
+                            amount NUMERIC(10, 2) CHECK (amount > 0),
+                            description TEXT, 
+                            date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+                            FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE CASCADE
+                        );''')
+        cursor.execute('''  CREATE TABLE IF NOT EXISTS daily_limits (
+                            id SERIAL PRIMARY KEY, 
+                            sub_account_id INTEGER NOT NULL,
+                            daily_limit NUMERIC(10, 2), 
+                            set_date DATE,
+                            changes_today INTEGER DEFAULT 0,
+                            FOREIGN KEY (sub_account_id) REFERENCES sub_account(id) ON DELETE CASCADE
+                        );''')
+        cursor.execute('''  CREATE TABLE IF NOT EXISTS sub_account (
+                            id SERIAL PRIMARY KEY,
+                            account_id INTEGER NOT NULL,
+                            currency CHAR(3) NOT NULL,
+                            balance NUMERIC(15, 2) DEFAULT 0 CHECK (balance >= 0),
+                            FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE CASCADE
+                        );''')
         connection.commit()
         cursor.close()
     except psycopg2.Error as e:
